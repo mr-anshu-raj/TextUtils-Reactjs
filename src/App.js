1c613555b2ef2852813f8b9c1c8db669c3c2b605
Anshu <73537420+mranshu-dev@users.noreply.github.com>
2023-02-17 17:37:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import './App.css';
+import Alert from './components/Alert';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+
+function App() {
+  const [Mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1000);
+  }
+
+  const togglemode = () => {
+    if(Mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor= '#23294a';
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor= 'white';
+      showAlert("Light mode has been enabled", "success");
+    }
+  }
+
+  return (
+    <>
+      <Router>
+      <Navbar title="Textutils" aboutText="About Textutils" Mode={Mode} togglemode={togglemode}/>
+      <Alert alert={alert}/>
+      <TextForm heading="Enter the text here" Mode={Mode} showAlert={showAlert}/>
+      <div className='container my-3'>
+        <Routes>
+          {/*<Route path="/about" element={<About />}/>*/}
+          {/*<Route path="/" element={}/>*/}
+        </Routes>
+      </div>
+      </Router>
+      
+    </>
+  );
+}
+
+export default App;
