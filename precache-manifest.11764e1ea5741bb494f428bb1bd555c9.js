a0ceea163fb8d391bbf272e453bb4444f1c9e6f1
Anshu <anshu.super.ar@gmail.com>
2023-02-17 17:00:35 +0530

    Updates

---
+++ precache-manifest.11764e1ea5741bb494f428bb1bd555c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8db6140d986f57589610",
+    "url": "/TextUtils-Reactjs/static/css/main.7865e8cb.chunk.css"
+  },
+  {
+    "revision": "8db6140d986f57589610",
+    "url": "/TextUtils-Reactjs/static/js/main.8db6140d.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/TextUtils-Reactjs/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "37eb27e2194863bfe39c",
+    "url": "/TextUtils-Reactjs/static/js/2.37eb27e2.chunk.js"
+  },
+  {
+    "revision": "966f3678d5bcbe22b530",
+    "url": "/TextUtils-Reactjs/static/js/runtime~main.966f3678.js"
+  },
+  {
+    "revision": "e5d47636f707d44d4dd5f86d0c193c71",
+    "url": "/TextUtils-Reactjs/index.html"
+  }
+];
