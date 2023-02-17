81fe9ff4fed30065f3f8c5f4836b83b49dae76cf
Anshu <anshu.super.ar@gmail.com>
2023-02-17 18:00:48 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/TextUtils-Reactjs/precache-manifest.11764e1ea5741bb494f428bb1bd555c9.js"
+  "/TextUtils-Reactjs/precache-manifest.e56a73ceccae1c2c4632f1f34f9886e2.js"
 );
 
 workbox.clientsClaim();
