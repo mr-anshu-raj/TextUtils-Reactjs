75a068084ff1e303b06cbb12b912d48ae28cab43
Anshu <anshu.super.ar@gmail.com>
2023-02-18 14:49:05 +0530

    Initial commit

---
+++ precache-manifest.0d609955cc420cf2593c5b0d1d297132.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4828212b86406a95ab7",
+    "url": "/TextUtils-Reactjs/static/css/main.7865e8cb.chunk.css"
+  },
+  {
+    "revision": "a4828212b86406a95ab7",
+    "url": "/TextUtils-Reactjs/static/js/main.a4828212.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/TextUtils-Reactjs/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "37eb27e2194863bfe39c",
+    "url": "/TextUtils-Reactjs/static/js/2.37eb27e2.chunk.js"
+  },
+  {
+    "revision": "966f3678d5bcbe22b530",
+    "url": "/TextUtils-Reactjs/static/js/runtime~main.966f3678.js"
+  },
+  {
+    "revision": "d5c925e8efab0b8edc20677264232119",
+    "url": "/TextUtils-Reactjs/index.html"
+  }
+];
